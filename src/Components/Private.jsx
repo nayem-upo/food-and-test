82e74034eafd30f931a072c836749fbe3a1b0a23
programmer-nayem <nayemislam0181@gmail.com>
2023-05-03 10:38:53 +0600

    private router added

--- src/Components/Private.jsx
+++ src/Components/Private.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Private = ({children}) => {
-    return (
-        <div>
-            
+const Private = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
+
+    const location = useLocation()
+
+    if (loading) {
+        return <div className='h-[600px] flex mb-96 bg-white justify-center'>
+            <progress className="progress md:w-[600px] w-[80%] my-auto"></progress>
         </div>
-    );
+    }
+
+    if (user) {
+        return children;
+    }
+    return <Navigate to="/login" state={{ from: location }} replace />
 };
 
 export default Private;
