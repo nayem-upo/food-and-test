2adb955bad2ea426122ec076428efee3d2c10074
programmer-nayem <nayemislam0181@gmail.com>
2023-05-03 22:25:42 +0600

    whole website responsive done

--- src/Components/ChefCard.jsx
+++ src/Components/ChefCard.jsx
@@ -20,11 +20,7 @@ const ChefCard = ({ chef }) => {
             <h1 className='text-xl font-semibold m-3'>{name}</h1>
             <p className='ms-3 font-semibold'>Experience: {years_of_experience} years</p>
             <hr className="mb-0 mt-3 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
-            <div className='flex justify-evenly'>
-                <p className='p-2'><FontAwesomeIcon className='text-[#60AA2D]' icon={faPlateWheat} /> Recipes: <span className='text-[#60AA2D] font-semibold'>{recipes_count}</span></p>
-                <div className="inline-block  min-h-[1em] w-0.5 self-stretch bg-neutral-100 opacity-100 dark:opacity-50"></div>
-                <p className='p-2'><FontAwesomeIcon className='text-[#60AA2D]' icon={faThumbsUp} /> Likes: <span className='text-[#60AA2D] font-semibold'>{likes}</span></p>
-            </div>
+            
             <button onClick={() => navigate(`/details/${id}`)} className='w-full bg-[#60AA2D] bottom-0 py-2 hover:bg-[#3b7b10] text-white font-semibold'>View Recipes</button>
         </div>
     );
